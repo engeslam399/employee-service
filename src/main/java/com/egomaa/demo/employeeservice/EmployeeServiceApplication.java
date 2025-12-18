32446d4ad64e0d09fa72d6a366c51cd11fe50bcc
Eslam Gomaa <engeslam399@gmail.com>
2025-12-19 01:54:55 +0200

    feat: initialize Spring Boot application and ignore log files.

--- src/main/java/com/egomaa/demo/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/egomaa/demo/employeeservice/EmployeeServiceApplication.java
@@ -8,6 +8,7 @@ public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
+        System.out.println("----- Employee Service Started Successfully -----");
     }
 
 }
